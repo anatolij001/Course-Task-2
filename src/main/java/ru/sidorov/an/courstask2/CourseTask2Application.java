802daf809fb2d2cedbeaf557b57585d2145f630e
anatolij <anatolij001@gmail.com>
2024-03-19 14:43:21 +0300

    Initial Commit.

---
+++ src/main/java/ru/sidorov/an/courstask2/CourseTask2Application.java
@@ -0,0 +1,13 @@
+package ru.sidorov.an.courstask2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseTask2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseTask2Application.class, args);
+	}
+
+}
